eb4167f843a0fbc2f6707bca07d7a0ad5fc5012a
CHIR4G7 <93506828+CHIR4G7@users.noreply.github.com>
2022-03-01 22:54:21 +0530

    Add files via upload

---
+++ 21105037_output.py
@@ -0,0 +1,89 @@
+Python 3.9.10 (v3.9.10:f2f3f53782, Jan 13 2022, 17:02:14) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /Users/chirag/Desktop/21105037_assg4.py ===============
+Q1
+Enter number of disk in tower of Hanoi:4
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 3 from Source Tower to Auxiliary
+Move disk 1 from Destination to Source Tower
+Move disk 2 from Destination to Auxiliary
+Move disk 1 from Source Tower to Auxiliary
+Move disk 4 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 2 from Auxiliary to Source Tower
+Move disk 1 from Destination to Source Tower
+Move disk 3 from Auxiliary to Destination
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Q2
+enter the no. of rows7
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+Q3
+
+Enter an Integer:27
+Enter another Integer:70
+
+The quotient when 27 is divided by 70 is 0.
+The remainder when 27 is divided by 70 is 27.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are NOT 'non-zero'
+
+Ques.c
+Appended (4,5,6) in the values (0,27)
+Values greater than 4 is 27,5,6
+
+Que3.d
+Above result in set form is shown below:
+{27, 5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 27
+Hash value of 27 is 27
+Q4
+enter name of student :chirag
+Enter SID of chirag: 21105037
+object created
+
+The name of the student is chirag and his/her roll number is 21105037
+object deleted
+Q5
+The data is stored where:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+All deleted, i.e. Viren
+
+Q6
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:chirag
+Enter Player2 name:ayush
+
+Enter Word by chirag:car
+Enter Word by ayush:race
+
+Friendship of chirag and ayush is FAKE
+>>> 
